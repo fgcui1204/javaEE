94f6f88da4509b392c74858a97f4129e5f8d9dd7
fgcui <fgcui1204@163.com>
2015-07-09 15:44:16 +0800

    remove unuse code

--- core/src/main/java/com/tw/dao/UserDao.java
+++ core/src/main/java/com/tw/dao/UserDao.java
@@ -5,19 +5,10 @@ import com.tw.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    int result = 0;
-
-    DbConnection dbConnection = new DbConnection();
-    Connection connection = dbConnection.getConnection();
-    Statement statement = null;//statement 用来执行sql语句
 
     public List<User> getUserList(){
         List<User> userList = new ArrayList<User>();
@@ -36,14 +27,6 @@ public class UserDao {
         session.save(user);
         transaction.commit();
         session.close();
-//        try {
-//            statement = connection.createStatement();
-//            String sql = "insert into users values (null,'"+user.getName()+"','"+user.getSex()+"','"+user.getMail()+"',"+user.getAge()+")";
-//            result = statement.executeUpdate(sql);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return result;
     }
 
     public void deleteUser(int id){
@@ -54,14 +37,6 @@ public class UserDao {
         session.delete(user);
         transaction.commit();
         session.close();
-//        try {
-//            statement = connection.createStatement();
-//            String sql = "delete from users where id="+id;
-//            result = statement.executeUpdate(sql);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return result;
     }
 
     public User getUser(int id){
@@ -69,26 +44,6 @@ public class UserDao {
 
         User user = (User)session.get(User.class,id);
         return user;
-//        User user = new User();
-//        try {
-//            statement = connection.createStatement();
-//            String sql = "select * from users where id="+id;
-//
-//            ResultSet resultSet = statement.executeQuery(sql);
-//
-//            while (resultSet.next()) {
-//                int userId = resultSet.getInt("id");
-//                String name = resultSet.getString("name");
-//                String sex = resultSet.getString("sex");
-//                String mail = resultSet.getString("mail");
-//                int age = resultSet.getInt("age");
-//
-//                user = new User(userId,name,sex,mail,age);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return user;
     }
 
     public void updateUser(User user){
@@ -99,14 +54,5 @@ public class UserDao {
         session.update(user);
         transaction.commit();
         session.close();
-//        try {
-//            statement = connection.createStatement();
-//            String sql = "update users set name='"+user.getName()+"',sex='"+user.getSex()+"',mail='"+user.getMail()+"',age="+user.getAge()+" where id="+user.getUserId();
-//
-//            result = statement.executeUpdate(sql);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return result;
     }
 }
