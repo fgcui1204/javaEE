46b2c5e1a2f809a6dd67eaebf07fa94a89b87039
fgcui <fgcui1204@163.com>
2015-07-09 12:02:17 +0800

    refactor UserDao and Service

--- core/src/main/java/com/tw/core/Service.java
+++ core/src/main/java/com/tw/core/Service.java
@@ -2,6 +2,7 @@ package com.tw.core;
 
 import com.tw.bean.User;
 import com.tw.dao.DbConnection;
+import com.tw.dao.UserDao;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -11,112 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
-    public String service() {
-        return "Hello World";
+    UserDao userDao = new UserDao();
+
+    public List<User> getUserList() {
+        return userDao.getUserList();
     }
 
-    public List<User> getUserList(){
-        DbConnection dbConnection = new DbConnection();
-
-        Connection connection = dbConnection.getConnection();
-        List<User> userList = new ArrayList<User>();
-
-        Statement statement = null;//statement 用来执行sql语句
-        try {
-            statement = connection.createStatement();
-            String sql = "select * from users";
-
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String sex = resultSet.getString("sex");
-                String mail = resultSet.getString("mail");
-                int age = resultSet.getInt("age");
-
-                User user = new User(id,name,sex,mail,age);
-                userList.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return userList;
+    public int addUser(User user) {
+        return userDao.addUser(user);
     }
 
-    public int addUser(User user){
-        int result = 0;
-        DbConnection dbConnection = new DbConnection();
-
-        Connection connection = dbConnection.getConnection();
-
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "insert into users values (null,'"+user.getName()+"','"+user.getSex()+"','"+user.getMail()+"',"+user.getAge()+")";
-            result = statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
+    public int deleteUser(int id) {
+        return userDao.deleteUser(id);
     }
 
-    public int deleteUser(int id){
-        int result = 0;
-        DbConnection dbConnection = new DbConnection();
-
-        Connection connection = dbConnection.getConnection();
-
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "delete from users where id="+id;
-            result = statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
+    public User getUser(int id) {
+        return userDao.getUser(id);
     }
 
-    public User getUser(int id){
-        User user = new User();
-        DbConnection dbConnection = new DbConnection();
-
-        Connection connection = dbConnection.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "select * from users where id="+id;
-
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                int userId = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String sex = resultSet.getString("sex");
-                String mail = resultSet.getString("mail");
-                int age = resultSet.getInt("age");
-
-                user = new User(userId,name,sex,mail,age);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-       return user;
+    public int updateUser(User user) {
+        return userDao.updateUser(user);
     }
-
-    public int updateUser(User user){
-        int result = 0;
-
-        DbConnection dbConnection = new DbConnection();
-
-        Connection connection = dbConnection.getConnection();
-
-        try {
-            Statement statement = connection.createStatement();
-            String sql = "update users set name='"+user.getName()+"',sex='"+user.getSex()+"',mail='"+user.getMail()+"',age="+user.getAge()+" where id="+user.getUserId();
-
-            result = statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-            return result;
     }
-}
--- core/src/main/java/com/tw/dao/UserDao.java
+++ core/src/main/java/com/tw/dao/UserDao.java
@@ -1,7 +1,103 @@
 package com.tw.dao;
 
-/**
- * Created by fgcui on 7/9/15.
- */
+import com.tw.bean.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDao {
+    int result = 0;
+
+    DbConnection dbConnection = new DbConnection();
+    Connection connection = dbConnection.getConnection();
+    Statement statement = null;//statement 用来执行sql语句
+
+    public List<User> getUserList(){
+        List<User> userList = new ArrayList<User>();
+
+        try {
+            statement = connection.createStatement();
+            String sql = "select * from users";
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String sex = resultSet.getString("sex");
+                String mail = resultSet.getString("mail");
+                int age = resultSet.getInt("age");
+
+                User user = new User(id,name,sex,mail,age);
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
+
+    public int addUser(User user){
+
+        try {
+            statement = connection.createStatement();
+            String sql = "insert into users values (null,'"+user.getName()+"','"+user.getSex()+"','"+user.getMail()+"',"+user.getAge()+")";
+            result = statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public int deleteUser(int id){
+
+        try {
+            statement = connection.createStatement();
+            String sql = "delete from users where id="+id;
+            result = statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public User getUser(int id){
+        User user = new User();
+        try {
+            statement = connection.createStatement();
+            String sql = "select * from users where id="+id;
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                int userId = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String sex = resultSet.getString("sex");
+                String mail = resultSet.getString("mail");
+                int age = resultSet.getInt("age");
+
+                user = new User(userId,name,sex,mail,age);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public int updateUser(User user){
+
+        try {
+            statement = connection.createStatement();
+            String sql = "update users set name='"+user.getName()+"',sex='"+user.getSex()+"',mail='"+user.getMail()+"',age="+user.getAge()+" where id="+user.getUserId();
+
+            result = statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
