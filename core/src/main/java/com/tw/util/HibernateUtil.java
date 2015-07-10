df52ca0c822c6bce9558f424ce09be8df2327944
fgcui <fgcui1204@163.com>
2015-07-10 15:33:49 +0800

    add some annotation

--- core/src/main/java/com/tw/util/HibernateUtil.java
+++ core/src/main/java/com/tw/util/HibernateUtil.java
@@ -10,7 +10,7 @@ public class HibernateUtil {
         Configuration configuration = new Configuration().configure();
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
         sessionFactory = configuration.buildSessionFactory(builder.build());
-    }
+    }//在其他代码执行之前会先被调用，而且只调用一次（单例模式）
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
