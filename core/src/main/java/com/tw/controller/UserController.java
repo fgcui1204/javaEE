46382edf54914cdcf0d466fc4a24ea9595d64173
fgcui <fgcui1204@163.com>
2015-07-12 19:47:44 +0800

    modify function about user management

--- core/src/main/java/com/tw/controller/UserController.java
+++ core/src/main/java/com/tw/controller/UserController.java
@@ -1,7 +1,56 @@
 package com.tw.controller;
 
-/**
- * Created by fgcui on 7/12/15.
- */
+import com.sun.net.ssl.HostnameVerifier;
+import com.tw.bean.User;
+import com.tw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getUserList(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("userManagement");
+        modelAndView.addObject("userList", userService.getUserList());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    public ModelAndView getUser(@RequestParam int id){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("updateUser");
+        modelAndView.addObject("user",userService.getUser(id));
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+    public ModelAndView updateUser(@RequestParam int id, @RequestParam String name,@RequestParam String sex, @RequestParam String mail, @RequestParam int age){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User(id, name,sex, mail,age);
+        userService.updateUser(user);
+        return this.getUserList();
+    }
+
+    @RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
+    public ModelAndView deleteUser(@RequestParam int id){
+        userService.deleteUser(id);
+        return this.getUserList();
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public ModelAndView addeUser(@RequestParam String name,@RequestParam String sex, @RequestParam String mail, @RequestParam int age){
+        User user = new User(name,sex, mail,age);
+        userService.addUser(user);
+        return this.getUserList();
+    }
+
 }
