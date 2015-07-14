e663b2caf7e6aecdd5ccaa0a8bbfdf8d796b3885
fgcui <fgcui1204@163.com>
2015-07-14 17:55:56 +0800

    add parse md5 funciton

--- core/src/main/java/com/tw/util/ParseMD5.java
+++ core/src/main/java/com/tw/util/ParseMD5.java
@@ -1,7 +1,33 @@
 package com.tw.util;
 
+import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by fgcui on 7/14/15.
  */
+@Service
 public class ParseMD5 {
+    public String parseStrToMd5L32(String str){
+        String reStr = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(str.getBytes());
+            StringBuffer stringBuffer = new StringBuffer();
+            for (byte b : bytes){
+                int bt = b&0xff;
+                if (bt < 16){
+                    stringBuffer.append(0);
+                }
+                stringBuffer.append(Integer.toHexString(bt));
+            }
+            reStr = stringBuffer.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return reStr;
+    }
+
 }
