ef1ce6d1e6ff144c4a04ddcc88ad15214a7e7524
fgcui <fgcui1204@163.com>
2015-07-14 16:22:56 +0800

    modify LoginIntercepter

--- core/src/main/java/com/tw/util/LoginInterceptor.java
+++ core/src/main/java/com/tw/util/LoginInterceptor.java
@@ -1,7 +1,41 @@
 package com.tw.util;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by fgcui on 7/14/15.
  */
-public class LoginInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取url地址
+        String reqUrl=request.getRequestURI().replace(request.getContextPath(), "");
+        //当url地址为登录的url的时候跳过拦截器
+        if(reqUrl.contains("/login")||reqUrl.contains("/session")){
+            return true;
+        }else{
+            HttpSession session=request.getSession();
+            Object obj=session.getAttribute("user");
+            if(obj==null||"".equals(obj.toString())){
+                response.sendRedirect("/web/users/login");
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }
