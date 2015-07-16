5e2e22747431968c8de1e5dc6f40ffc6047097bc
fgcui <fgcui1204@163.com>
2015-07-16 15:00:00 +0800

    add init data

---
+++ core/src/main/resources/db/seed/init_data.sql
@@ -0,0 +1,3 @@
+insert into users VALUES (null,'崔福罡','男','fgcui@outlook.com',23,'e10adc3949ba59abbe56e057f20f883e');
+insert into users VALUES (null,'小明','男','xmwang@thoughtworks.com',23,'e10adc3949ba59abbe56e057f20f883e');
+insert into users VALUES (null,'Q','女','qxue@thoughtworks.com',23,'e10adc3949ba59abbe56e057f20f883e');
