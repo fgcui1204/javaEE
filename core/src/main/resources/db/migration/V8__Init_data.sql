2d7a9bbaf986ed57f533f5c3ff6f007fcb7eb075
fgcui <fgcui1204@163.com>
2015-07-20 11:17:56 +0800

    add init data

--- core/src/main/resources/db/migration/V8__Init_data.sql
+++ core/src/main/resources/db/migration/V8__Init_data.sql
@@ -3,3 +3,18 @@ INSERT INTO employees VALUES (NULL ,'HR', 2);
 INSERT INTO employees VALUES (NULL ,'Coach', 3);
 INSERT INTO employees VALUES (NULL ,'Coach', 5);
 INSERT INTO employees VALUES (NULL ,'Coach', 6);
+
+INSERT INTO courses VALUES (NULL ,'体操');
+INSERT INTO courses VALUES (NULL ,'瑜伽');
+INSERT INTO courses VALUES (NULL ,'杠铃');
+INSERT INTO courses VALUES (NULL ,'短跑');
+INSERT INTO courses VALUES (NULL ,'私教');
+
+INSERT INTO customers VALUES (NULL ,'顾客1','123456');
+INSERT INTO customers VALUES (NULL ,'顾客3','123456');
+INSERT INTO customers VALUES (NULL ,'顾客2','123456');
+
+INSERT INTO customer_course VALUES (NULL , 1, 1);
+INSERT INTO customer_course VALUES (NULL , 1, 2);
+INSERT INTO customer_course VALUES (NULL , 2, 4);
+INSERT INTO customer_course VALUES (NULL , 3, 3);
