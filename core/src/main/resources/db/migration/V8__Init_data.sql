fcb286d7388378708c13453495881637e2fd1893
fgcui <fgcui1204@163.com>
2015-07-20 09:40:28 +0800

    modify init data into employeee

---
+++ core/src/main/resources/db/migration/V8__Init_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees VALUES (NULL ,'OPs', 1);
+INSERT INTO employees VALUES (NULL ,'HR', 2);
+INSERT INTO employees VALUES (NULL ,'Coach', 3);
+INSERT INTO employees VALUES (NULL ,'Coach', 5);
+INSERT INTO employees VALUES (NULL ,'Coach', 6);
